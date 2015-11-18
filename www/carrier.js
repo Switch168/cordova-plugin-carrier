88778f5b5b73a45b83610b4854c114031c8f030c
roir <roi@bringg.com>
2015-11-18 18:16:51 +0200

    Update carrier.js

--- www/carrier.js
+++ www/carrier.js
@@ -1,6 +1,9 @@
 var carrier = {
   getCarrierInfo: function (successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, 'Carrier', 'getCarrierInfo', []);
+  },
+  geocodeCountryCode: function (lat, lng, successCallback, errorCallback){
+    cordova.exec(successCAllback, errorCallback, 'Carrier', 'geocodeCountryCode', [lat, lng]);
   }
 }
 
